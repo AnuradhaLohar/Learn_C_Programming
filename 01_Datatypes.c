24221399ccd37097a12279ecbd9441c0a08251d2
AnuradhaLohar <loharanuradha644@gmail.com>
2025-01-30 17:19:32 +0530

    Datatype in c

---
+++ 01_Datatypes.c
@@ -0,0 +1,72 @@
+        #include<stdio.h>
+        #include<limits.h>
+
+        int main()
+        {
+        //     char ch = 65;
+        //     printf("%d", ch);
+        //    printf("%c", ch);//they return ASCII value of 65 means 65A
+
+        // float f = 95;
+        // printf("%f", f);//output is 95.000000
+        // printf("%.2f", f); // output 95.00 .2f means assign .after 2 digits 
+
+        // printf( "\'Hello World \'"); // 'Hello World'
+
+        //  unsigned int j = -10;
+        // printf("%d", j); // output -10
+        //  signed int j = -10;
+        // printf("%d", j);
+
+        // double d = 10.33;
+        // printf("%.2lf", d);//10.33
+
+        // int i;
+        // printf("%d\n", INT_MAX);// 214783647
+        // printf("%d", INT_MIN); //-2147483648
+
+        // printf("%d\n", CHAR_MAX);//127
+        // printf("%d\n", CHAR_MIN);//-128
+
+        // printf("%d\n", FLOAT_MAX);//ERROR
+        // printf("%d\n", FLOAT_MIN);
+
+        // int num = 10;
+        // printf("Enter value of num = ");
+        //  scanf("%d", &num);
+        //  printf("%d" , num);
+        //  return 0;
+
+        // double num;
+        // printf("%lf\n", num);// output garbeg value 2146304
+        // printf("%d", &num); //they give value of address of num
+
+        // char ch;
+        // printf("Enter value of ch = ");
+        // scanf("%c", &ch);
+        // printf("%c", ch);// entered value of num is anu tehy return output is a
+
+        // float f;
+        // printf("\nEnter value of f = ");
+        // scanf("%f", &f);
+        // printf("%f", f);
+
+
+        // double d;
+        // printf("\nEnter value of d = ");
+        // scanf("%lf", &d);
+        // printf("%lf", d);
+
+        // int i;
+        // float var;
+        // double d;
+        // char ch;
+        // printf("Enter value of i , var, d, ch = ");
+        // scanf("%d  %f %lf %c", &i, &var, &d, &ch);
+        // printf("%d  %f %lf %c", i, var, d, ch);
+        // return 0;
+
+
+
+
+        }
