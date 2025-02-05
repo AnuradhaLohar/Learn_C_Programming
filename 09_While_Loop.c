6f3f5bf891d364a4fc3f56bd2eb6bfc71b616f1d
AnuradhaLohar <loharanuradha644@gmail.com>
2025-02-05 08:54:51 +0530

    while loop

---
+++ 09_While_Loop.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    // int i = 0;
+    // while(i < 5)
+    // {
+    //     printf("Hello World\n");
+    //     i++;
+    // }
+
+    // int i = 1;
+    // int j = 0;
+    // while(i <= 10){
+    //     j = 2 * i;
+    //     printf("2 * %d = %d\n",i,j);
+    //     i++;   
+    // }
+
+    int i = 10;
+    int j = 0;
+    while(i >= 1){
+        j = 2 * i;
+        printf("2 * %d = %d\n",i,j);
+        i--;
+    }
+
+
+}
